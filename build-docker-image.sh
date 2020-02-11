5dda6c27353d5373d16519a95f7645ca394ec8d1
Andy Grove <andygrove73@gmail.com>
2020-02-10 21:43:41 -0700

    archive everything

--- build-docker-image.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-REPO=andygrove
-
-docker build -t ${REPO}/ballista-platform -f docker/Dockerfile.platform .
-docker build -t ${REPO}/ballista -f docker/Dockerfile.binary --build-arg REPO=${REPO} .

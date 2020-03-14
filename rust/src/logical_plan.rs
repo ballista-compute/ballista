e251ffdc2b5ba0e4df563b8195da4391cf98c109
Andy Grove <andygrove73@gmail.com>
2020-03-14 09:33:38 -0600

    plan builder

--- rust/src/logical_plan.rs
+++ rust/src/logical_plan.rs
@@ -20,7 +20,22 @@ pub enum LogicalExpr {
 }
 
 #[derive(Debug,Clone)]
+pub enum LogicalAggregateExpr {
+    Min(LogicalExpr),
+    Max(LogicalExpr),
+    Sum(LogicalExpr),
+    Avg(LogicalExpr),
+    Count(LogicalExpr),
+    CountDistinct(LogicalExpr),
+}
+
+#[derive(Debug,Clone)]
 pub enum LogicalPlan {
+    Aggregate {
+        group_expr: Vec<LogicalExpr>,
+        aggr_expr: Vec<LogicalAggregateExpr>,
+        input: Box<LogicalPlan>,
+    },
     Projection {
         expr: Vec<LogicalExpr>,
         input: Box<LogicalPlan>,
@@ -34,10 +49,57 @@ pub enum LogicalPlan {
     }
 }
 
+trait Relation {
+    fn project(&self, expr: Vec<LogicalExpr>) -> Box<dyn Relation>;
+    fn filter(&self, expr: LogicalExpr) -> Box<dyn Relation>;
+}
+
+struct LogicalPlanBuilder {
+    plan: LogicalPlan
+}
+
+impl LogicalPlanBuilder {
+
+    fn from(plan: LogicalPlan) -> Self {
+        Self { plan }
+    }
+
+    fn project(&self, expr: Vec<LogicalExpr>) -> LogicalPlanBuilder {
+        LogicalPlanBuilder::from(LogicalPlan::Projection {
+            expr,
+            input: Box::new(self.plan.clone())
+        })
+    }
+
+    fn filter(&self, expr: LogicalExpr) -> LogicalPlanBuilder {
+        LogicalPlanBuilder::from(LogicalPlan::Selection {
+            expr: Box::new(expr),
+            input: Box::new(self.plan.clone())
+        })
+    }
+
+    fn build(&self) -> LogicalPlan {
+        self.plan.clone()
+    }
+}
+
+fn col(name: &str) -> LogicalExpr {
+    LogicalExpr::Column(name.to_owned())
+}
+fn lit_str(str: &str) -> LogicalExpr {
+    LogicalExpr::LiteralString(str.to_owned())
+}
+
+fn eq(l: LogicalExpr, r: LogicalExpr) -> LogicalExpr {
+    LogicalExpr::Eq(Box::new(l), Box::new(r))
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     use super::LogicalPlan::*;
     use super::LogicalExpr::*;
+    use crate::logical_plan::LogicalPlanBuilder;
 
     #[test]
     fn build_plan_manually() {
@@ -46,22 +108,12 @@ mod tests {
             filename: "employee.csv".to_owned()
         };
 
-        let filter = Selection {
-            input: Box::new(scan),
-            expr: Box::new(Eq(
-                Box::new(Column("state".to_owned())),
-                Box::new(LiteralString("CO".to_owned())),
-            ))
-        };
+        let plan = LogicalPlanBuilder::from(scan)
+            .filter(eq(col("state"), lit_str("CO")))
+            .project(vec![col("state")])
+            .build();
 
-        let projection = Projection {
-            input: Box::new(filter),
-            expr: vec![
-                Column("state".to_owned())
-            ],
-        };
-
-        let plan_str = format!("{:?}", projection);
+        let plan_str = format!("{:?}", plan);
         let expected_str =
             "Projection { \
                 expr: [Column(\"state\")], \

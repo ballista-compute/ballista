e78bdeeeb35b6c6ebf12b10ed5f4711c26b4a226
Andy Grove <andygrove73@gmail.com>
2020-03-17 21:24:31 -0600

    save

---
+++ examples/distributed-query-rust/build-docker-image.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir -p temp/ballista/proto
+cp ../../proto/ballista.proto temp/ballista/proto/
+
+mkdir -p temp/ballista/rust
+cp -rf ../../rust/* temp/ballista/rust
+
+
+
+
+
--- examples/distributed-query-rust/src/main.rs
+++ examples/distributed-query-rust/src/main.rs
@@ -1,26 +1,17 @@
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-
 use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::sync::Arc;
 
 use arrow::array::Int32Array;
-use arrow::datatypes::Schema;
+use arrow::datatypes::{DataType, Field, Schema};
 use arrow::flight::flight_data_to_batch;
+
+use ballista::protobuf;
+use datafusion::logicalplan::*;
+
+use prost::bytes::BufMut;
+use prost::Message;
+
 //use flight::flight_descriptor;
 use flight::flight_service_client::FlightServiceClient;
 use flight::Ticket;
@@ -29,9 +20,29 @@ use flight::Ticket;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = FlightServiceClient::connect("http://localhost:50051").await?;
 
-    let request = tonic::Request::new(Ticket {
-        ticket: "SELECT id FROM alltypes_plain".into(),
-    });
+    let plan = LogicalPlanBuilder::scan(
+        "default",
+        "employee",
+        &Schema::new(vec![
+            Field::new("id", DataType::Utf8, false),
+            Field::new("first_name", DataType::Utf8, false),
+            Field::new("last_name", DataType::Utf8, false),
+            Field::new("state", DataType::Utf8, false),
+            Field::new("salary", DataType::UInt32, false),
+        ]),
+        None,
+    )
+        .and_then(|plan| plan.filter(col(0).eq(&lit_str("CO"))))
+        .and_then(|plan| plan.project(&vec![col(0)]))
+        .and_then(|plan| plan.build())
+        //.map_err(|e| Err(format!("{:?}", e)))
+        .unwrap(); //TODO
+
+    let serialized_plan: protobuf::LogicalPlanNode = plan.try_into().unwrap();
+    let mut buf: Vec<u8> = Vec::with_capacity(serialized_plan.encoded_len());
+    serialized_plan.encode(&mut buf).unwrap();
+
+    let request = tonic::Request::new(Ticket { ticket: buf });
 
     let mut stream = client.do_get(request).await?.into_inner();
 

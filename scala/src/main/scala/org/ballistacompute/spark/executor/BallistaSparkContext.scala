13c72befb9c5bf92c2330fa56d394cef070257b8
Andy Grove <andygrove73@gmail.com>
2020-04-09 08:12:02 -0600

    Start point for BallistaSparkContext

--- scala/src/main/scala/org/ballistacompute/spark/executor/BallistaSparkContext.scala
+++ scala/src/main/scala/org/ballistacompute/spark/executor/BallistaSparkContext.scala
@@ -1,20 +1,42 @@
 package org.ballistacompute.spark.executor
 
-import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.ballistacompute.logical.{LogicalPlan, Scan}
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
+import org.ballistacompute.logical.{LogicalExpr, LogicalPlan, Projection, Scan}
 
 import scala.collection.JavaConverters._
 
 class BallistaSparkContext(spark: SparkSession) {
 
-  def foo(plan: LogicalPlan): DataFrame = {
+  /** Translate Ballista logical plan step into a DataFrame transformation */
+  def createDataFrame(plan: LogicalPlan, input: Option[DataFrame]): DataFrame = {
 
     plan match {
+
       case s: Scan =>
+        assert(input.isEmpty)
         val df = spark.read.parquet(s.getName)
         val projection: Seq[String] = s.getProjection().asScala
-        df.select(projection: _*)
+        df.select(projection.head, projection.tail: _*)
 
+      case p: Projection =>
+        val df = input.get
+        val expr = p.getExpr.asScala.map(e => createExpression(e, input.get))
+        df.select(expr: _*)
+
+      case other =>
+        throw new UnsupportedOperationException(s"Ballista logical plan step can not be converted to Spark: $other")
+    }
+
+  }
+
+  /** Translate Ballista logical expression into a Spark logical expression */
+  def createExpression(e: LogicalExpr, input: DataFrame): Column = {
+    e match {
+      case c: org.ballistacompute.logical.Column =>
+        input.col(c.getName)
+
+      case other =>
+        throw new UnsupportedOperationException(s"Ballista logical expression can not be converted to Spark: $other")
     }
 
   }

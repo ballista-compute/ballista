04e50f28ef1a552d470ec9e1767ee556fd0158e5
Andy Grove <andygrove73@gmail.com>
2020-05-07 21:08:30 -0600

    save

--- benchmarks/run-parquet.sh
+++ benchmarks/run-parquet.sh
@@ -1,20 +1,4 @@
 #!/usr/bin/env bash
-cargo run -- --bench=spark --format=csv --path=/mnt/nyctaxi/csv/year=2019 --cpus=3
-cargo run -- --bench=rust --format=csv --path=/mnt/nyctaxi/csv/year=2019 --cpus=3
-cargo run -- --bench=jvm --format=csv --path=/mnt/nyctaxi/csv/year=2019 --cpus=3
-
-cargo run -- --bench=spark --format=csv --path=/mnt/nyctaxi/csv/year=2019 --cpus=6
-cargo run -- --bench=rust --format=csv --path=/mnt/nyctaxi/csv/year=2019 --cpus=6
-cargo run -- --bench=jvm --format=csv --path=/mnt/nyctaxi/csv/year=2019 --cpus=6
-
-cargo run -- --bench=spark --format=csv --path=/mnt/nyctaxi/csv/year=2019 --cpus=9
-cargo run -- --bench=rust --format=csv --path=/mnt/nyctaxi/csv/year=2019 --cpus=9
-cargo run -- --bench=jvm --format=csv --path=/mnt/nyctaxi/csv/year=2019 --cpus=9
-
-cargo run -- --bench=spark --format=csv --path=/mnt/nyctaxi/csv/year=2019 --cpus=12
-cargo run -- --bench=rust --format=csv --path=/mnt/nyctaxi/csv/year=2019 --cpus=12
-cargo run -- --bench=jvm --format=csv --path=/mnt/nyctaxi/csv/year=2019 --cpus=12
-
 cargo run -- --bench=spark --format=parquet --path=/mnt/nyctaxi/parquet/year=2019 --cpus=3
 cargo run -- --bench=rust --format=parquet --path=/mnt/nyctaxi/parquet/year=2019 --cpus=3
 #cargo run -- --bench=jvm --format=parquet --path=/mnt/nyctaxi/parquet/year=2019 --cpus=3

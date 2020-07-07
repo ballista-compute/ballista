0daf362f3613eb3cc858ad813f26b89e06c6d0d7
Andy Grove <andygrove73@gmail.com>
2020-07-06 21:43:54 -0600

    clean up test

--- rust/ballista/tests/query_execution.rs
+++ rust/ballista/tests/query_execution.rs
@@ -3,24 +3,22 @@ extern crate ballista;
 use ballista::error::Result;
 use ballista::execution::hash_aggregate::HashAggregateExec;
 use ballista::execution::parquet_scan::ParquetScanExec;
-use ballista::execution::physical_plan::{
-    AggregateMode, ColumnarBatchStream, ExecutionPlan, PhysicalPlan,
-};
+use ballista::execution::physical_plan::{AggregateMode, ColumnarBatchStream, PhysicalPlan};
 
 use std::rc::Rc;
 
 // #[test]
-// fn query() -> Result<()> {
+// fn hash_aggregate() -> Result<()> {
 //     let path = nyc_path();
-//     let parquet = ParquetScanExec::try_new(&path, None)?;
-//     let hash_agg = HashAggregateExec::new(
+//     let parquet = PhysicalPlan::ParquetScan(Rc::new(ParquetScanExec::try_new(&path, None)?));
+//     let hash_agg = PhysicalPlan::HashAggregate(Rc::new(HashAggregateExec::new(
 //         AggregateMode::Partial,
 //         vec![],
 //         vec![],
-//         Rc::new(PhysicalPlan::ParquetScan(Rc::new(parquet))),
-//     );
+//         Rc::new(parquet),
+//     )));
 //
-//     let stream: ColumnarBatchStream = hash_agg.execute(0)?;
+//     let stream: ColumnarBatchStream = hash_agg.as_execution_plan().execute(0)?;
 //     while let Some(batch) = stream.next()? {
 //         println!(
 //             "batch with {} rows and {} columns",

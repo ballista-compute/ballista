7baf987aa0f43c72584cbeef17263f58bf3e5cf2
Patrick More <patrickjmore@gmail.com>
2021-01-26 16:11:48 -0700

    Reverted to previous build.rs version

--- rust/ballista/build.rs
+++ rust/ballista/build.rs
@@ -14,8 +14,6 @@
 
 fn main() -> Result<(), String> {
     println!("cargo:rerun-if-changed=proto/ballista.proto");
-    prost_build::Config::new()
-        .out_dir("src/serde/")
-        .compile_protos(&["proto/ballista.proto"], &["proto"])
+    prost_build::compile_protos(&["proto/ballista.proto"], &["proto"])
         .map_err(|e| format!("protobuf compilation failed: {}", e))
 }

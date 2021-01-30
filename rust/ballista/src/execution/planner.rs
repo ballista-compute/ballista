91b30654fe68e2a48aedc42643e6e38ecbc76fd4
Andy Grove <andygrove73@gmail.com>
2021-01-30 12:45:23 -0700

    Implement to_proto for Action

--- rust/ballista/src/execution/planner.rs
+++ rust/ballista/src/execution/planner.rs
@@ -87,166 +87,166 @@ fn pretty_print(plan: Arc<dyn ExecutionPlan>, indent: usize) {
         .for_each(|c| pretty_print(c.clone(), indent + 1));
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use crate::context::BallistaContext;
-//     use arrow::datatypes::{DataType, DateUnit, Field, Schema};
-//     use datafusion::execution::context::ExecutionContext;
-//     use datafusion::physical_plan::csv::CsvReadOptions;
-//     use std::collections::HashMap;
-//
-//     #[tokio::test]
-//     async fn test() -> Result<()> {
-//         // assumes benchmark data is available
-//         let tpch_data = "../../benchmarks/tpch/data";
-//
-//         let ctx = BallistaContext::remote("localhost", 50051, HashMap::new());
-//
-//         let tables = vec!["lineitem", "orders"];
-//         for table in &tables {
-//             let schema = get_schema(table);
-//             let options = CsvReadOptions::new()
-//                 .schema(&schema)
-//                 .file_extension(".tbl")
-//                 .has_header(false)
-//                 .delimiter(b'|');
-//             ctx.register_csv(table, &format!("{}/{}.tbl", tpch_data, table), options)?;
-//         }
-//
-//         let plan = ctx.sql(
-//             "select
-//     l_shipmode,
-//     sum(case
-//             when o_orderpriority = '1-URGENT'
-//                 or o_orderpriority = '2-HIGH'
-//                 then 1
-//             else 0
-//         end) as high_line_count,
-//     sum(case
-//             when o_orderpriority <> '1-URGENT'
-//                 and o_orderpriority <> '2-HIGH'
-//                 then 1
-//             else 0
-//         end) as low_line_count
-// from
-//     lineitem
-//         join
-//     orders
-//     on
-//             l_orderkey = o_orderkey
-// where
-//         l_shipmode in ('MAIL', 'SHIP')
-//   and l_commitdate < l_receiptdate
-//   and l_shipdate < l_commitdate
-//   and l_receiptdate >= date '1994-01-01'
-//   and l_receiptdate < date '1995-01-01'
-// group by
-//     l_shipmode
-// order by
-//     l_shipmode",
-//         )?;
-//
-//         let df = ExecutionContext::new();
-//         let plan = df.optimize(&plan.to_logical_plan())?;
-//         let plan = df.create_physical_plan(&plan)?;
-//         execute_distributed_query(plan).await?;
-//
-//         Ok(())
-//     }
-//
-//     fn get_schema(table: &str) -> Schema {
-//         // note that the schema intentionally uses signed integers so that any generated Parquet
-//         // files can also be used to benchmark tools that only support signed integers, such as
-//         // Apache Spark
-//
-//         match table {
-//             "part" => Schema::new(vec![
-//                 Field::new("p_partkey", DataType::Int32, false),
-//                 Field::new("p_name", DataType::Utf8, false),
-//                 Field::new("p_mfgr", DataType::Utf8, false),
-//                 Field::new("p_brand", DataType::Utf8, false),
-//                 Field::new("p_type", DataType::Utf8, false),
-//                 Field::new("p_size", DataType::Int32, false),
-//                 Field::new("p_container", DataType::Utf8, false),
-//                 Field::new("p_retailprice", DataType::Float64, false),
-//                 Field::new("p_comment", DataType::Utf8, false),
-//             ]),
-//
-//             "supplier" => Schema::new(vec![
-//                 Field::new("s_suppkey", DataType::Int32, false),
-//                 Field::new("s_name", DataType::Utf8, false),
-//                 Field::new("s_address", DataType::Utf8, false),
-//                 Field::new("s_nationkey", DataType::Int32, false),
-//                 Field::new("s_phone", DataType::Utf8, false),
-//                 Field::new("s_acctbal", DataType::Float64, false),
-//                 Field::new("s_comment", DataType::Utf8, false),
-//             ]),
-//
-//             "partsupp" => Schema::new(vec![
-//                 Field::new("ps_partkey", DataType::Int32, false),
-//                 Field::new("ps_suppkey", DataType::Int32, false),
-//                 Field::new("ps_availqty", DataType::Int32, false),
-//                 Field::new("ps_supplycost", DataType::Float64, false),
-//                 Field::new("ps_comment", DataType::Utf8, false),
-//             ]),
-//
-//             "customer" => Schema::new(vec![
-//                 Field::new("c_custkey", DataType::Int32, false),
-//                 Field::new("c_name", DataType::Utf8, false),
-//                 Field::new("c_address", DataType::Utf8, false),
-//                 Field::new("c_nationkey", DataType::Int32, false),
-//                 Field::new("c_phone", DataType::Utf8, false),
-//                 Field::new("c_acctbal", DataType::Float64, false),
-//                 Field::new("c_mktsegment", DataType::Utf8, false),
-//                 Field::new("c_comment", DataType::Utf8, false),
-//             ]),
-//
-//             "orders" => Schema::new(vec![
-//                 Field::new("o_orderkey", DataType::Int32, false),
-//                 Field::new("o_custkey", DataType::Int32, false),
-//                 Field::new("o_orderstatus", DataType::Utf8, false),
-//                 Field::new("o_totalprice", DataType::Float64, false),
-//                 Field::new("o_orderdate", DataType::Date32(DateUnit::Day), false),
-//                 Field::new("o_orderpriority", DataType::Utf8, false),
-//                 Field::new("o_clerk", DataType::Utf8, false),
-//                 Field::new("o_shippriority", DataType::Int32, false),
-//                 Field::new("o_comment", DataType::Utf8, false),
-//             ]),
-//
-//             "lineitem" => Schema::new(vec![
-//                 Field::new("l_orderkey", DataType::Int32, false),
-//                 Field::new("l_partkey", DataType::Int32, false),
-//                 Field::new("l_suppkey", DataType::Int32, false),
-//                 Field::new("l_linenumber", DataType::Int32, false),
-//                 Field::new("l_quantity", DataType::Float64, false),
-//                 Field::new("l_extendedprice", DataType::Float64, false),
-//                 Field::new("l_discount", DataType::Float64, false),
-//                 Field::new("l_tax", DataType::Float64, false),
-//                 Field::new("l_returnflag", DataType::Utf8, false),
-//                 Field::new("l_linestatus", DataType::Utf8, false),
-//                 Field::new("l_shipdate", DataType::Date32(DateUnit::Day), false),
-//                 Field::new("l_commitdate", DataType::Date32(DateUnit::Day), false),
-//                 Field::new("l_receiptdate", DataType::Date32(DateUnit::Day), false),
-//                 Field::new("l_shipinstruct", DataType::Utf8, false),
-//                 Field::new("l_shipmode", DataType::Utf8, false),
-//                 Field::new("l_comment", DataType::Utf8, false),
-//             ]),
-//
-//             "nation" => Schema::new(vec![
-//                 Field::new("n_nationkey", DataType::Int32, false),
-//                 Field::new("n_name", DataType::Utf8, false),
-//                 Field::new("n_regionkey", DataType::Int32, false),
-//                 Field::new("n_comment", DataType::Utf8, false),
-//             ]),
-//
-//             "region" => Schema::new(vec![
-//                 Field::new("r_regionkey", DataType::Int32, false),
-//                 Field::new("r_name", DataType::Utf8, false),
-//                 Field::new("r_comment", DataType::Utf8, false),
-//             ]),
-//
-//             _ => unimplemented!(),
-//         }
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::context::BallistaContext;
+    use arrow::datatypes::{DataType, DateUnit, Field, Schema};
+    use datafusion::execution::context::ExecutionContext;
+    use datafusion::physical_plan::csv::CsvReadOptions;
+    use std::collections::HashMap;
+
+    #[tokio::test]
+    async fn test() -> Result<()> {
+        // assumes benchmark data is available
+        let tpch_data = "../../benchmarks/tpch/data";
+
+        let ctx = BallistaContext::remote("localhost", 50051, HashMap::new());
+
+        let tables = vec!["lineitem", "orders"];
+        for table in &tables {
+            let schema = get_schema(table);
+            let options = CsvReadOptions::new()
+                .schema(&schema)
+                .file_extension(".tbl")
+                .has_header(false)
+                .delimiter(b'|');
+            ctx.register_csv(table, &format!("{}/{}.tbl", tpch_data, table), options)?;
+        }
+
+        let plan = ctx.sql(
+            "select
+    l_shipmode,
+    sum(case
+            when o_orderpriority = '1-URGENT'
+                or o_orderpriority = '2-HIGH'
+                then 1
+            else 0
+        end) as high_line_count,
+    sum(case
+            when o_orderpriority <> '1-URGENT'
+                and o_orderpriority <> '2-HIGH'
+                then 1
+            else 0
+        end) as low_line_count
+from
+    lineitem
+        join
+    orders
+    on
+            l_orderkey = o_orderkey
+where
+        l_shipmode in ('MAIL', 'SHIP')
+  and l_commitdate < l_receiptdate
+  and l_shipdate < l_commitdate
+  and l_receiptdate >= date '1994-01-01'
+  and l_receiptdate < date '1995-01-01'
+group by
+    l_shipmode
+order by
+    l_shipmode",
+        )?;
+
+        let df = ExecutionContext::new();
+        let plan = df.optimize(&plan.to_logical_plan())?;
+        let plan = df.create_physical_plan(&plan)?;
+        execute_distributed_query(plan).await?;
+
+        Ok(())
+    }
+
+    fn get_schema(table: &str) -> Schema {
+        // note that the schema intentionally uses signed integers so that any generated Parquet
+        // files can also be used to benchmark tools that only support signed integers, such as
+        // Apache Spark
+
+        match table {
+            "part" => Schema::new(vec![
+                Field::new("p_partkey", DataType::Int32, false),
+                Field::new("p_name", DataType::Utf8, false),
+                Field::new("p_mfgr", DataType::Utf8, false),
+                Field::new("p_brand", DataType::Utf8, false),
+                Field::new("p_type", DataType::Utf8, false),
+                Field::new("p_size", DataType::Int32, false),
+                Field::new("p_container", DataType::Utf8, false),
+                Field::new("p_retailprice", DataType::Float64, false),
+                Field::new("p_comment", DataType::Utf8, false),
+            ]),
+
+            "supplier" => Schema::new(vec![
+                Field::new("s_suppkey", DataType::Int32, false),
+                Field::new("s_name", DataType::Utf8, false),
+                Field::new("s_address", DataType::Utf8, false),
+                Field::new("s_nationkey", DataType::Int32, false),
+                Field::new("s_phone", DataType::Utf8, false),
+                Field::new("s_acctbal", DataType::Float64, false),
+                Field::new("s_comment", DataType::Utf8, false),
+            ]),
+
+            "partsupp" => Schema::new(vec![
+                Field::new("ps_partkey", DataType::Int32, false),
+                Field::new("ps_suppkey", DataType::Int32, false),
+                Field::new("ps_availqty", DataType::Int32, false),
+                Field::new("ps_supplycost", DataType::Float64, false),
+                Field::new("ps_comment", DataType::Utf8, false),
+            ]),
+
+            "customer" => Schema::new(vec![
+                Field::new("c_custkey", DataType::Int32, false),
+                Field::new("c_name", DataType::Utf8, false),
+                Field::new("c_address", DataType::Utf8, false),
+                Field::new("c_nationkey", DataType::Int32, false),
+                Field::new("c_phone", DataType::Utf8, false),
+                Field::new("c_acctbal", DataType::Float64, false),
+                Field::new("c_mktsegment", DataType::Utf8, false),
+                Field::new("c_comment", DataType::Utf8, false),
+            ]),
+
+            "orders" => Schema::new(vec![
+                Field::new("o_orderkey", DataType::Int32, false),
+                Field::new("o_custkey", DataType::Int32, false),
+                Field::new("o_orderstatus", DataType::Utf8, false),
+                Field::new("o_totalprice", DataType::Float64, false),
+                Field::new("o_orderdate", DataType::Date32(DateUnit::Day), false),
+                Field::new("o_orderpriority", DataType::Utf8, false),
+                Field::new("o_clerk", DataType::Utf8, false),
+                Field::new("o_shippriority", DataType::Int32, false),
+                Field::new("o_comment", DataType::Utf8, false),
+            ]),
+
+            "lineitem" => Schema::new(vec![
+                Field::new("l_orderkey", DataType::Int32, false),
+                Field::new("l_partkey", DataType::Int32, false),
+                Field::new("l_suppkey", DataType::Int32, false),
+                Field::new("l_linenumber", DataType::Int32, false),
+                Field::new("l_quantity", DataType::Float64, false),
+                Field::new("l_extendedprice", DataType::Float64, false),
+                Field::new("l_discount", DataType::Float64, false),
+                Field::new("l_tax", DataType::Float64, false),
+                Field::new("l_returnflag", DataType::Utf8, false),
+                Field::new("l_linestatus", DataType::Utf8, false),
+                Field::new("l_shipdate", DataType::Date32(DateUnit::Day), false),
+                Field::new("l_commitdate", DataType::Date32(DateUnit::Day), false),
+                Field::new("l_receiptdate", DataType::Date32(DateUnit::Day), false),
+                Field::new("l_shipinstruct", DataType::Utf8, false),
+                Field::new("l_shipmode", DataType::Utf8, false),
+                Field::new("l_comment", DataType::Utf8, false),
+            ]),
+
+            "nation" => Schema::new(vec![
+                Field::new("n_nationkey", DataType::Int32, false),
+                Field::new("n_name", DataType::Utf8, false),
+                Field::new("n_regionkey", DataType::Int32, false),
+                Field::new("n_comment", DataType::Utf8, false),
+            ]),
+
+            "region" => Schema::new(vec![
+                Field::new("r_regionkey", DataType::Int32, false),
+                Field::new("r_name", DataType::Utf8, false),
+                Field::new("r_comment", DataType::Utf8, false),
+            ]),
+
+            _ => unimplemented!(),
+        }
+    }
+}
